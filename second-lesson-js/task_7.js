513902be655a20d23db7e2d77d03d5a6d6d71970
artem <mikanev03@mail.ru>
2022-10-15 18:58:48 +0300

    homework done

---
+++ second-lesson-js/task_7.js
@@ -0,0 +1,59 @@
+//Ответ на Ваше задание: "хакуна матата", ключ = 8
+// for (let i = 0; i <= 10; i++){
+//     console.log(cesar("эзтыхз фзъзъз",i,"decode") + "  ", i); 
+// }
+
+function cesar(str, shift, action){
+    //думал через .charCodeAt(), но там проблемы с "ё" и "Ё"
+    let result = "";
+    let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" //33
+    if (action == "encode"){
+        for (let character of str){
+            if (alphabet.indexOf(character.toLowerCase())>-1){
+                if (character == character.toUpperCase()){
+                    result += alphabet[(alphabet.indexOf(character.toLowerCase()) + shift) % 33].toUpperCase();
+                }
+                else {
+                    result += alphabet[(alphabet.indexOf(character) + shift) % 33];
+                }
+            }
+            else{
+                result += character;
+            }
+        }
+    }
+    else if(action == "decode"){
+        for (let character of str){
+            if (alphabet.indexOf(character.toLowerCase())>-1){
+                if (character == character.toUpperCase()){
+                    result += alphabet.at((alphabet.indexOf(character.toLowerCase()) - shift) % 33).toUpperCase();
+                }
+                else {
+                    result += alphabet.at((alphabet.indexOf(character) - shift) % 33);
+                }
+            }
+            else{
+                result += character;
+            }
+        }
+    }
+    return result;
+}
+
+
+console.log("<-------ТЕСТЫ------->");
+
+let encode = cesar("Привет!",31,"encode");
+console.log(encode);
+
+let decode = cesar(encode,31,"decode");
+console.log(decode);
+
+// let message = "Привет!";
+
+// for (let i = 0; i<100; i++){
+//     if (message != cesar(cesar(message,i,"encode"),i,"decode")){
+//         console.log(i);
+//     }
+// }
+
